828bf01a9a1cb1cc425f2db1029f38d9ee2b960c
Anurag Vinodh <avinodh@edu.uwaterloo.ca>
2022-01-04 19:01:39 -0500

    Edit_User Page, Vide Call Menu, Video Call Page

--- app/src/main/java/com/example/binder/ui/EditUserFragment.kt
+++ app/src/main/java/com/example/binder/ui/EditUserFragment.kt
@@ -1,16 +1,34 @@
 package com.example.binder.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
+import com.example.binder.databinding.LayoutEditUserFragmentBinding
+import com.example.binder.databinding.LayoutHubFragmentBinding
 import data.Config
+import data.EditUserConfig
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import viewmodel.EditUserFragmentViewModel
 import viewmodel.MainActivityViewModel
 
-class EditUserFragment(override val config: Config) : BaseFragment() {
+class EditUserFragment(override val config: EditUserConfig) : BaseFragment() {
+
+    override val viewModel: ViewModel by viewModel<EditUserFragmentViewModel>()
 
     private val mainActivityViewModel by sharedViewModel<MainActivityViewModel>()
 
-    override val viewModel: ViewModel by viewModel<EditUserFragmentViewModel>()
+    private var binding: LayoutEditUserFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = LayoutEditUserFragmentBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
 
 }
