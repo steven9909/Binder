4899567054d6b916b52450d3a36c917532707f88
Eric <erickim8488@gmail.com>
2022-01-06 00:44:09 -0500

    another messaging rough draft

--- app/src/main/java/repository/RealtimeDB.kt
+++ app/src/main/java/repository/RealtimeDB.kt
@@ -1,29 +1,60 @@
 package repository
 
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import data.Message
-import resultCatching
 
 
 class RealtimeDB(val fb: Firebase) {
 
     private val MESSAGES = "Messages"
+    private val PAGESIZE = 50
 
-    fun sendMessage(message: Message, groupID: String) {
-        fb.database.getReference(MESSAGES).child(groupID).push().setValue(message)
+    fun sendMessage(message: Message, uid: String) {
+        fb.database.getReference(MESSAGES).child(uid).push().setValue(message)
     }
 
-    fun getMessages(groupID: String) {
-        fb.database.getReference(MESSAGES).child(groupID).get().addOnSuccessListener {
-            if (it.exists()) {
-                val messages = listOf(it.getValue(Message::class.java))
-            } else {
-                throw GetMessageFailException
-            }
-        }.addOnFailureListener {
-            throw GetMessageFailException
-        }
+    fun getMessage(uid: String) {
+        fb.database
+            .getReference(MESSAGES)
+            .child(uid)
+            .orderByChild("sentTime")
+            .limitToLast(PAGESIZE)
+            .addChildEventListener(object: ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    val msg = snapshot.getValue(Message::class.java)
+
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    //Do Nothing
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    //Do Nothing
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    //Do Nothing
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    //Do Nothing
+                }
+        })
+    }
+
+    fun getMoreMessages(uid: String, messageList: List<Message>) {
+        fb.database
+            .getReference(MESSAGES)
+            .child(uid)
+            .orderByChild("sentTime")
+            .endAt(messageList[messageList.size - 1].sentTime.toString())
+            .limitToLast(PAGESIZE)
+            .get()
     }
 }
 
