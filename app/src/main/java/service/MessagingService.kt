22590963654597b768f70b6ca37ee32109084f02
Benny Huang <37185367+JBennyHuang@users.noreply.github.com>
2022-01-07 23:08:34 -0500

    Added some template while awaiting Eric's code

--- app/src/main/java/service/MessagingService.kt
+++ app/src/main/java/service/MessagingService.kt
@@ -1,7 +1,18 @@
 package service
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
-    // logic here
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // logic here
+    }
+
+    override fun onNewToken(token: String) {
+        uploadRegistrationTokenToFirestore(token)
+    }
+
+    private fun uploadRegistrationTokenToFirestore(token: String) {
+        // TODO: Eric implement this
+    }
 }
