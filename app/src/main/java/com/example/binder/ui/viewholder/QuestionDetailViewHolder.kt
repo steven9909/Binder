5f9598d95757c6e5381aeb7c2cd7325962e202b9
Eric <erickim8488@gmail.com>
2022-01-24 01:39:12 -0500

    fix

--- app/src/main/java/com/example/binder/ui/viewholder/QuestionDetailViewHolder.kt
+++ app/src/main/java/com/example/binder/ui/viewholder/QuestionDetailViewHolder.kt
@@ -2,11 +2,13 @@ package com.example.binder.ui.viewholder
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.example.binder.R
 import com.example.binder.databinding.LayoutQuestionDetailViewHolderBinding
 import com.example.binder.ui.Item
 import com.example.binder.ui.OnActionListener
 
-class QuestionDetailViewHolder(parent: ViewGroup, listener: OnActionListener) : BaseViewHolder<Item>(
+class QuestionDetailViewHolder(parent: ViewGroup,
+                               listener: OnActionListener) : BaseViewHolder<Item>(
     listener,
     LayoutQuestionDetailViewHolderBinding.inflate(
         LayoutInflater.from(parent.context),
@@ -16,6 +18,11 @@ class QuestionDetailViewHolder(parent: ViewGroup, listener: OnActionListener) : 
     override val type: Int
         get() = ViewHolderFactory.QUESTION_DETAIL_TYPE
 
+    private var isClicked1: Boolean = false
+    private var isClicked2: Boolean = false
+    private var isClicked3: Boolean = false
+    private var isClicked4: Boolean = false
+
     override fun bindView(item: Item) {
         (item as? QuestionDetailItem)?.let {
             (binding as? LayoutQuestionDetailViewHolderBinding)?.let { binding ->
@@ -24,12 +31,108 @@ class QuestionDetailViewHolder(parent: ViewGroup, listener: OnActionListener) : 
                 binding.answer2Text.text = it.answers[1]
                 binding.answer3Text.text = it.answers[2]
                 binding.answer4Text.text = it.answers[3]
+                binding.questionText.setBackgroundColor(context.getColor(R.color.white))
+                binding.answer1Text.setBackgroundColor(context.getColor(R.color.white))
+                binding.answer2Text.setBackgroundColor(context.getColor(R.color.white))
+                binding.answer3Text.setBackgroundColor(context.getColor(R.color.white))
+                binding.answer4Text.setBackgroundColor(context.getColor(R.color.white))
+
+                binding.questionText.setOnClickListener {
+                    binding.questionText.setBackgroundColor(context.getColor(R.color.green))
+                    if (item.correctIndexes[0] == 0) {
+                        binding.answer1Text.setBackgroundColor(context.getColor(R.color.green))
+                    }
+                    if (item.correctIndexes[1] == 1) {
+                        binding.answer2Text.setBackgroundColor(context.getColor(R.color.green))
+                    }
+                    if (item.correctIndexes[2] == 2) {
+                        binding.answer3Text.setBackgroundColor(context.getColor(R.color.green))
+                    }
+                    if (item.correctIndexes[3] == 3) {
+                        binding.answer4Text.setBackgroundColor(context.getColor(R.color.green))
+                    }
+                }
+
+                binding.answer1Text.setOnClickListener {
+                    isClicked1 = true
+                    when (isClicked1) {
+                        true -> {
+                            if (item.correctIndexes[0] == 0) {
+                                binding.answer1Text.setBackgroundColor(context.getColor(R.color.green))
+                                binding.questionText.setBackgroundColor(context.getColor(R.color.green))
+                            } else {
+                                binding.answer1Text.setBackgroundColor(context.getColor(R.color.red))
+                            }
+                        }
+                        false -> {
+                            binding.answer1Text.setBackgroundColor(context.getColor(R.color.white))
+                        }
+                    }
+                }
+
+                binding.answer2Text.setOnClickListener {
+                    isClicked2 = true
+                    when (isClicked2) {
+                        true -> {
+                            if (item.correctIndexes[1] == 1) {
+                                binding.answer2Text.setBackgroundColor(context.getColor(R.color.green))
+                                binding.questionText.setBackgroundColor(context.getColor(R.color.green))
+                            } else {
+                                binding.answer2Text.setBackgroundColor(context.getColor(R.color.red))
+                            }
+                        }
+                        false -> {
+                            binding.answer2Text.setBackgroundColor(context.getColor(R.color.white))
+                        }
+                    }
+                }
+
+                binding.answer3Text.setOnClickListener {
+                    isClicked3 = true
+                    when (isClicked3) {
+                        true -> {
+                            if (item.correctIndexes[2] == 2) {
+                                binding.answer3Text.setBackgroundColor(context.getColor(R.color.green))
+                                binding.questionText.setBackgroundColor(context.getColor(R.color.green))
+                            } else {
+                                binding.answer3Text.setBackgroundColor(context.getColor(R.color.red))
+                            }
+                        }
+                        false -> {
+                            binding.answer3Text.setBackgroundColor(context.getColor(R.color.white))
+                        }
+                    }
+                }
+
+                binding.answer4Text.setOnClickListener {
+                    isClicked4 = true
+                    when (isClicked4) {
+                        true -> {
+                            if (item.correctIndexes[3] == 3) {
+                                binding.answer4Text.setBackgroundColor(context.getColor(R.color.green))
+                                binding.questionText.setBackgroundColor(context.getColor(R.color.green))
+                            } else {
+                                binding.answer4Text.setBackgroundColor(context.getColor(R.color.red))
+                            }
+                        }
+                        false -> {
+                            binding.answer4Text.setBackgroundColor(context.getColor(R.color.white))
+                        }
+                    }
+                }
             }
         }
     }
 
     override fun recycle() {
-        Unit
+        isClicked1 = false
+        isClicked2 = false
+        isClicked3 = false
+        isClicked4 = false
+        (binding as? LayoutQuestionDetailViewHolderBinding)?.answer1Text?.setBackgroundColor(context.getColor(R.color.white))
+        (binding as? LayoutQuestionDetailViewHolderBinding)?.answer2Text?.setBackgroundColor(context.getColor(R.color.white))
+        (binding as? LayoutQuestionDetailViewHolderBinding)?.answer3Text?.setBackgroundColor(context.getColor(R.color.white))
+        (binding as? LayoutQuestionDetailViewHolderBinding)?.answer4Text?.setBackgroundColor(context.getColor(R.color.white))
     }
 }
 
