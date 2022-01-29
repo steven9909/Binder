119b0ea6a7f5e5b08ba454adcb3fea53391a7cdc
Tzu-Chun Chou <t6chou@uwaterloo.ca>
2022-01-30 07:15:32 +0800

    setup localization

--- app/src/main/java/com/example/binder/ui/MainActivity.kt
+++ app/src/main/java/com/example/binder/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.binder.ui
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.binder.R
@@ -15,6 +19,7 @@ import data.LoginConfig
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import viewmodel.MainActivityViewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,8 +34,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadLocale()
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -83,6 +90,24 @@ class MainActivity : AppCompatActivity() {
             return false
         }
         return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setLocale(localeToSet: String) {
+        val localeListToSet = LocaleList(Locale(localeToSet))
+        LocaleList.setDefault(localeListToSet)
+        resources.configuration.setLocales(localeListToSet)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        val sharedPref = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        sharedPref.putString("locale_to_set", localeToSet)
+        sharedPref.apply()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadLocale() {
+        val sharedPref = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        val localeToSet: String = sharedPref.getString("locale_to_set", "")!!
+        setLocale(localeToSet)
     }
 
     fun getNameFromGoogleSignIn(): String =
--- app/src/main/java/com/example/binder/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/binder/ui/fragment/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.binder.ui.fragment
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModel
 import com.example.binder.databinding.LayoutFriendFinderFragmentBinding
+import com.example.binder.databinding.LayoutSettingsBinding
 import com.example.binder.ui.fragment.BaseFragment
 import data.FriendFinderConfig
 import data.InfoConfig
@@ -16,18 +19,25 @@ import viewmodel.LoginFragmentViewModel
 
 class SettingsFragment(override val config: SettingsConfig) : BaseFragment() {
 
-    private var binding: LayoutFriendFinderFragmentBinding? = null
+    private var binding: LayoutSettingsBinding? = null
 
     override val viewModel: ViewModel by viewModel<FriendFinderFragmentViewModel>()
+
+    private val langauge = arrayOf("English", "Korean")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = LayoutFriendFinderFragmentBinding.inflate(inflater, container, false)
-
+        binding = LayoutSettingsBinding.inflate(inflater, container, false)
+        setupUi()
         return binding!!.root
     }
 
-
+    private fun setupUi(){
+        binding?.let { binding ->
+            val spinner = binding.settingsLanguageSpinner
+            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,langauge)
+        }
 }
