2a2a99cddffc7a878f5f7233241360d43137f9dd
Steven Hyun <30786246+steven9909@users.noreply.github.com>
2022-02-27 19:40:08 -0500

    Steven/fix more (#57)
    * fix
    
    * detekt

--- app/src/main/java/viewmodel/LoginFragmentViewModel.kt
+++ app/src/main/java/viewmodel/LoginFragmentViewModel.kt
@@ -1,5 +1,8 @@
 package viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.binder.ui.usecase.DoesUserExistUseCase
 
-class LoginFragmentViewModel : BaseViewModel()
+class LoginFragmentViewModel(private val doesUserExistUseCase: DoesUserExistUseCase) : BaseViewModel() {
+    fun doesUserExist() = doesUserExistUseCase.getData()
+}
